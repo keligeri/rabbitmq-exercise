a6349979b3f3691a72b540beec3a13b0c688844e
keligeri <kel.gergo@gmail.com>
2018-01-07 18:17:12 +0100

    Done with tutorial2

---
+++ src/main/java/com/cc/rabbit/tut2/config/Tut2Config.java
@@ -0,0 +1,42 @@
+package com.cc.rabbit.tut2.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.cc.rabbit.tut2.receiver.Tut2Receiver;
+import com.cc.rabbit.tut2.sender.Tut2Sender;
+
+@Profile({"tut2", "work-queues"})
+@Configuration
+public class Tut2Config {
+
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    private static class ReceiverConfig {
+
+        @Bean
+        public Tut2Receiver receiver1() {
+            return new Tut2Receiver(1);
+        }
+
+        @Bean
+        public Tut2Receiver receiver2() {
+            return new Tut2Receiver(2);
+        }
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut2Sender sender() {
+        return new Tut2Sender();
+    }
+
+
+}
