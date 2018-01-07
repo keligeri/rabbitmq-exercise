31a36048b1708743b4f31be4cd71677df38467fe
keligeri <kel.gergo@gmail.com>
2018-01-07 18:54:31 +0100

    tut3 done

---
+++ src/main/java/com/cc/rabbit/tut3/config/Tut3Config.java
@@ -0,0 +1,62 @@
+package com.cc.rabbit.tut3.config;
+
+import com.cc.rabbit.tut3.receiver.Tut3Receiver;
+import com.cc.rabbit.tut3.sender.Tut3Sender;
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"tut3", "pub-sub", "publish-subscribe"})
+@Configuration
+public class Tut3Config {
+
+    @Bean
+    public FanoutExchange fanout() {
+        return new FanoutExchange("tut.fanout");
+    }
+
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    private static class ReceiverConfig {
+        @Bean
+        public Queue autoDeleteQueue1() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Queue autoDeleteQueue2() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Binding binding1(FanoutExchange fanout,
+                                Queue autoDeleteQueue1) {
+            return BindingBuilder.bind(autoDeleteQueue1).to(fanout);
+        }
+
+        @Bean
+        public Binding binding2(FanoutExchange fanout,
+                                Queue autoDeleteQueue2) {
+            return BindingBuilder.bind(autoDeleteQueue2).to(fanout);
+        }
+
+        @Bean
+        public Tut3Receiver receiver() {
+            return new Tut3Receiver();
+        }
+
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut3Sender sender() {
+        return new Tut3Sender();
+    }
+
+
+}
